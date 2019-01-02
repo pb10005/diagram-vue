a2a55554e3eda8177bc4d9b63c567993440b8e08
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-02 19:27:48 +0900

    Update sample data

--- src/data.js
+++ src/data.js
@@ -43,7 +43,7 @@ export default {
       width: 200,
       height: 60,
       shape: "ellipse",
-      point: { x: 683, y: 491.5452292961393 }
+      point: { x: 712, y: 542.5452292961393 }
     },
     {
       id: "167efae64ea50b14",
@@ -80,49 +80,57 @@ export default {
       id: "167efaaddec32dd0",
       source: "167efaa2c59c27a1",
       destination: "167efa699c7863ac",
-      point: { x: 1156.5, y: 172.62461249002126 }
+      point: { x: 1156.5, y: 172.62461249002126 },
+      color: "#55efc4"
     },
     {
       id: "167efabd659483de",
       source: "167efabc40ee0852",
       destination: "167efa988e882161",
-      point: { x: 345, y: 138.74688965089885 }
+      point: { x: 345, y: 138.74688965089885 },
+      color: "#74b9ff"
     },
     {
       id: "167efabe6bfc5f86",
       source: "167efabc40ee0852",
       destination: "167efa7fc5d2f8d6",
-      point: { x: 281, y: 254.73236332876465 }
+      point: { x: 281, y: 254.73236332876465 },
+      color: "#74b9ff"
     },
     {
       id: "167efae7330e886a",
       source: "167efae64ea50b14",
       destination: "167efae23b74ae81",
-      point: { x: 903.5, y: 474.16118118550173 }
+      point: { x: 951.5, y: 509.16118118550173 },
+      color: "#55efc4"
     },
     {
       id: "167efae7f84c412e",
       source: "167efae64ea50b14",
       destination: "167efaa2c59c27a1",
-      point: { x: 999.5, y: 234.90269774736257 }
+      point: { x: 920.5, y: 229.90269774736257 },
+      color: "#55efc4"
     },
     {
       id: "167efaf767b32352",
       source: "167efaf41e0cc6f2",
       destination: "167efaefb4058b44",
-      point: { x: 354.5, y: 444.7362925785632 }
+      point: { x: 354.5, y: 444.7362925785632 },
+      color: "#74b9ff"
     },
     {
       id: "167efafb77b76f76",
       source: "167efafa9d4b4dbc",
       destination: "167efabc40ee0852",
-      point: { x: 631, y: 226.1104724696181 }
+      point: { x: 631, y: 226.1104724696181 },
+      color: "#74b9ff"
     },
     {
       id: "167efafd0d253d23",
       source: "167efaf41e0cc6f2",
       destination: "167efafa9d4b4dbc",
-      point: { x: 526.5, y: 354.4701429988721 }
+      point: { x: 527.5, y: 354.4701429988721 },
+      color: "#74b9ff"
     }
   ]
 };
