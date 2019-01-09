7bd5643382168dbb686f1a062eb1b3738aea8ce1
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-10 01:19:51 +0900

    Update EditNodeModal.vue

--- src/lib/EditNodeModal.vue
+++ src/lib/EditNodeModal.vue
@@ -5,7 +5,7 @@
         <VInput v-model="newData.text" placeholder="name" /><br />
         <VInput v-model="newData.url" placeholder="url" /><br />
         <VInput v-model="newData.color" placeholder="color" /><br />
-        <VSelect v-model="newShape" multiple>
+        <VSelect v-model="newShape" placeholder="shape">
           <option value="rectangle" selected>Rectangle</option>
           <option value="ellipse">Ellipse</option> </VSelect
         ><br />
