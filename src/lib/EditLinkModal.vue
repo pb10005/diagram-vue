e5a4748829031270daab317cffbdccd1e113ef21
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-10 01:16:10 +0900

    Update EditLinkModal.vue

--- src/lib/EditLinkModal.vue
+++ src/lib/EditLinkModal.vue
@@ -3,7 +3,7 @@
     <transition name="item">
       <div class="form" v-if="isActive">
         <VInput v-model="content.color" placeholder="color" /><br />
-        <VSelect v-model="content.pattern" placeholder="pattern">
+        <VSelect v-model="content.pattern" placeholder="pattern" multiple>
           <option value="solid" selected>Solid</option>
           <option value="dash">Dash</option>
           <option value="dot">Dot</option> </VSelect
