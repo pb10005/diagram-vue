a5ca9ed4a02802225e662cfc8e09791091915dcd
Darlan <darlandieterich@gmail.com>
2019-10-04 14:58:08 -0300

    Added new modal to ask if wanna clear the diagram

---
+++ src/lib/AskModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <VModal :isActive="isActive" @clickModal="cancel">
+    <transition name="item">
+      <div class="block">
+        <div class="inner-block">
+            <slot></slot>
+        </div>
+        <VButton @click="ok">OK</VButton>
+        <VButton class="danger" @click="cancel">Cancel</VButton>
+      </div>
+    </transition>
+  </VModal>
+</template>
+<script>
+export default {
+  props: {
+    isActive: Boolean
+  },
+  methods: {
+    ok() {
+      this.$emit("ok", true);
+    },
+    cancel() {
+      this.$emit("cancel");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.inner-block{
+    margin-bottom: 20px;
+}
+.block {
+    padding: 40px;
+}
+input {
+  margin-bottom: 5px;
+}
+.item-enter-active {
+  transition: all 0.8s ease;
+}
+.item-leave-active {
+  transition: all 0.3s ease;
+}
+.item-enter,
+.item-leave-to {
+  opacity: 0;
+}
+</style>
