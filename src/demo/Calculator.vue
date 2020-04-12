82ff9a7345d15e02551cf721472f439d90f9a9e8
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2020-04-12 15:53:12 +0900

    Draft calculator

--- src/demo/Calculator.vue
+++ src/demo/Calculator.vue
@@ -1,6 +1,132 @@
 <template>
-  <section>Coming soon...</section>
+  <section>
+    <VSelect v-model="tmp.operandType" placeholder="Operand type">
+      <option value="constant">Constant</option>
+      <option value="variable">Variable</option>
+    </VSelect>
+    <VInput v-model="tmp.operandName" placeholder="name" />
+    <VInput v-model="tmp.operandValue" placeholder="value" />
+    <VButton @click="addOperand">Add operand</VButton><br />
+
+    <VSelect v-model="tmp.operatorType" placeholder="Operand type">
+      <option value="add">Add</option> <option value="sub">Sub</option>
+      <option value="mul">Mul</option> <option value="div">Div</option>
+    </VSelect>
+    <VButton @click="addOperator">Add operator</VButton>
+    <Diagram
+      :width="500"
+      :height="500"
+      background="#FF0"
+      :nodes="nodes"
+      :links="links"
+      :editable="true"
+      :labels="{
+        edit: 'Edit',
+        remove: 'Remove',
+        link: 'New Link',
+        select: 'Select',
+        cancel: 'Cancel',
+        copy: '_'
+      }"
+      @editNode="onEditNode"
+      @editLink="onEditLink"
+      @nodeChanged="nodeChanged"
+      @linkChanged="linkChanged"
+    >
+    </Diagram>
+  </section>
 </template>
 <script>
-export default {};
+import Diagram from "../index.js";
+export default {
+  components: {
+    Diagram
+  },
+  data() {
+    return {
+      tmp: {
+        operandType: "constant",
+        operandName: "x",
+        operandValue: 0,
+        operatorType: "add"
+      },
+      maxID: 0,
+      nodes: [],
+      links: [],
+      operators: [],
+      operands: []
+    };
+  },
+  methods: {
+    addOperand() {
+      this.maxID++;
+      this.operands.push({
+        id: this.maxID,
+        type: this.tmp.operandType,
+        value: this.tmp.operandValue
+      });
+      this.nodes.push({
+        id: this.maxID,
+        width: 100,
+        height: 50,
+        shape: "rectangle",
+        content: {
+          text: this.tmp.operandName
+        },
+        point: {
+          x: 10,
+          y: 10
+        }
+      });
+    },
+    addOperator() {
+      const resultID = ++this.maxID;
+      this.operands.push({
+        id: resultID
+      });
+      this.nodes.push({
+        id: resultID,
+        width: 100,
+        height: 50,
+        shape: "rectangle",
+        content: {
+        color: "#F00",
+          text: "result"
+        },
+        point: {
+          x: 20,
+          y: 50
+        }
+      });
+      const operatorID = ++this.maxID;
+      this.nodes.push({
+        id: this.maxID,
+        width: 100,
+        height: 50,
+        shape: "ellipse",
+        content: {
+          text: this.tmp.operatorType
+        },
+        point: {
+          x: 20,
+          y: 10
+        }
+      });
+      this.links.push({
+        id: ++this.maxID,
+        source: operatorID,
+        destination: resultID,
+        arrow: "dest",
+        point: {
+          x: 20,
+          y: 10
+        }
+      });
+    },
+    onEditNode() {},
+    onEditLink() {},
+    nodeChanged() {},
+    linkChanged() {}
+  }
+};
 </script>
--- src/lib/Link.vue
+++ src/lib/Link.vue
@@ -19,11 +19,11 @@
       "
     />
     <line
-       v-else
-        :x1="calcSource().x"
-        :y1="calcSource().y"
-        :x2="calcDestination().x"
-        :y2="calcDestination().y"
+      v-else
+      :x1="calcSource().x"
+      :y1="calcSource().y"
+      :x2="calcDestination().x"
+      :y2="calcDestination().y"
       :stroke="link.color || '#ffeaa7'"
       stroke-width="3"
       fill="none"
@@ -34,7 +34,7 @@
       :marker-end="
         link.arrow === 'dest' || link.arrow === 'both' ? `url(#${link.id})` : ''
       "
-      />
+    />
     <marker
       :id="link.id"
       markerUnits="userSpaceOnUse"
@@ -214,7 +214,7 @@ export default {
         id: this.link.id,
         content: {
           color: this.link.color || "#ffeaa7",
-          shapfe: this.link.shape || "straight",
+          shape: this.link.shape || "straight",
           pattern: this.link.pattern || "solid",
           arrow: this.link.arrow || "none"
         }
